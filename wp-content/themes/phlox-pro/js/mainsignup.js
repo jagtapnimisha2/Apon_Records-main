b894249d1c80a13853cc17fc067e59b77ad674e4
Deepanshu Kushwaha <deep.kush2631@gmail.com>
2025-01-18 00:49:16 +0530

    Appon_Record

---
+++ wp-content/themes/phlox-pro/js/mainsignup.js
@@ -0,0 +1,17 @@
+// const namee = document.getElementsById('username');
+// const lname=document.getElementsById('lname');
+// const step1=document.getElementsById('step1');
+// const usernameError =document.getElementById('usernameError');
+// const passwords=document.getElementById('passwords');
+
+// step1.addEventListener('submit',(e)=>{
+//     if(namee.value === '' || namee.value==null){
+//         e.preventDefault();
+//         usernameError.innerHTML = "name is requreds"
+//     }
+
+//     if(lname.value.length <= 5){
+//         e.preventDefault();
+//         passwords.innerHTML = "posward must be less than 6 characters"
+//     }
+// })
